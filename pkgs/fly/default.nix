b4c9be34b702af102282ad56b7561c9d92fc0ef2
Michael McKibben <mikemckibben@gmail.com>
2023-11-20 09:44:42 -0500

    adds fly7 binary pinned to v7.6.0

--- pkgs/fly/default.nix
+++ pkgs/fly/default.nix
@@ -21,5 +21,27 @@
         mv "$out/bin/fly" "$out/bin/fly6"
       '';
     };
+
+    extraPackages.fly7 = with pkgs; buildGoModule rec {
+      pname = "fly";
+      version = "7.6.0";
+      src =  fetchFromGitHub {
+        owner = "concourse";
+        repo = "concourse";
+        rev = "v${version}";
+        hash = "sha256-Zi+gyO+2AKDgcfgYrzLskJYZ6hQKOVlOL7Y9nxH/pGg=";
+      };
+      doCheck = false;
+      subPackages = [ "fly" ];
+      vendorSha256 = "sha256-OF3parnlTPmcr7tVcc6495sUMRApSpBHHjSE/4EFIxE=";
+      # vendorSha256 = lib.fakeSha256;
+      ldflags= [
+        "-X github.com/concourse/concourse.Version=${version}"
+      ];
+
+      postInstall = ''
+        mv "$out/bin/fly" "$out/bin/fly7"
+      '';
+    };
   };
 }
